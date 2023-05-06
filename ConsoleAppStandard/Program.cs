a777092cb79b58254afa245d582a56b261ef97e8
tushar <tusharkarsan@gmail.com>
2023-05-06 23:10:13 +0100

    Add project files.

---
+++ ConsoleAppStandard/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleAppStandard
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ ConsoleAppTwisted/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleAppTwisted
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
